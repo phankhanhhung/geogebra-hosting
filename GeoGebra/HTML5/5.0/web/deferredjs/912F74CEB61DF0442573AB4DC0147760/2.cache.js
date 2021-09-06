28b5e6d8e4c50320940cf24dc5a331a805f3002a
phankhanhhung <phankhanhhung@gmail.com>
2021-09-07 06:28:45 +0700

    add folder

---
+++ GeoGebra/HTML5/5.0/web/deferredjs/912F74CEB61DF0442573AB4DC0147760/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.web.runAsyncCallback2('hEc(3276,1,Wrm);_.xe=function(){};bEl(Pi)(2);\n//# sourceURL=web-2.js\n')
---
+++ GeoGebra/HTML5/5.0/webSimple/deferredjs/615B6B293F3767BF4F9A8FCB50E117D0/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.webSimple.runAsyncCallback2('dEb(1991,1,oPh);_.Hd=function(){};Pdh(eh)(2);\n//# sourceURL=webSimple-2.js\n')
